c69a71f15b19bb50b8a56e60c6329597ebfd0f3f
Eric Brose <ericbrose@gmail.com>
2022-10-17 09:49:45 -0700

    create tests

--- myApp.py
+++ myApp.py
@@ -3,9 +3,11 @@
 
 
 
-def addItUp(){
+def addItUp():
+    total = sum(range(5))
+    print(f"Total: ", total)
+    assert sum([1, 2, 3, 4, 5]) == 15, "Should be 6"
 
-}
 
 
 if __name__ == "__main__":
