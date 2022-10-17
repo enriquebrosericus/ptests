c9f75bbc13b2fb91f5eb226d3bb89d4fe599a2ca
Eric Brose <ericbrose@gmail.com>
2022-10-17 09:55:27 -0700

    tweak to test

--- myApp.py
+++ myApp.py
@@ -6,7 +6,7 @@
 def addItUp():
     total = sum(range(5))
     print(f"Total: ", total)
-    assert sum([1, 2, 3, 4, 5]) == 15, "Should be 6"
+    assert sum([2, 2, 3, 4, 5]) == 15, "Should be 6"
 
 
 
